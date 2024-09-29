2985772518c503c081e1d7bf0b8e509b939b4137
Partha Sarathi Bhunia <parthabhunia2001@gmail.com>
2024-09-30 00:29:45 +0530

    [Security] init security config
    Voila! Dummy Works..
    Added 3 beans for security configs

---
+++ src/main/java/com/chat/talke/config/SecurityConfig.java
@@ -0,0 +1,37 @@
+package com.chat.talke.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsManager(PasswordEncoder encoder) {
+        UserDetails user = User.withUsername("user")
+                .password(encoder.encode("1234"))
+                .roles("MEMBER").build();
+
+        return new InMemoryUserDetailsManager(user);
+    }
+
+    @Bean
+    public SecurityFilterChain filterChains(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity.authorizeHttpRequests(request -> request.anyRequest().authenticated()).httpBasic(Customizer.withDefaults()).build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
