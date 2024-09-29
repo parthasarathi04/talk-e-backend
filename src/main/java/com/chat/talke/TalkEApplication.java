2da4bbbd6647bb3b7780fd77cfd87ae5893ad24a
Partha Sarathi Bhunia <parthabhunia2001@gmail.com>
2024-09-29 23:09:17 +0530

    init backend

---
+++ src/main/java/com/chat/talke/TalkEApplication.java
@@ -0,0 +1,13 @@
+package com.chat.talke;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TalkEApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TalkEApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chat/talke/TalkEApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chat.talke;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TalkEApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
